d9c6c5ce6aaa53564654edfeeb7a43ebae51e5e7
Ian Miller <theiandmiller@gmail.com>
2022-08-23 05:20:31 -0500

    Bar Chart project

--- BarChart/index.js
+++ BarChart/index.js
@@ -1,155 +1,154 @@
-const projectName = "bar-chart";
+import * as d3 from "https://cdn.skypack.dev/d3@6.7.0";
 
-var width = 800;
-var height = 400;
-var barWidth = width / 275;
+// establish sizes
+const wide = 780;
+const high = 540;
+const barWidth = wide / 250;
 
+// get the data
+function getData(v, i) {
+   return v[i];
+}
+
+// find the max values
+function findMax(arr) {
+   return d3.max(arr);
+}
+
+// create the SVG
+var svg = d3
+   .select("#data")
+   .append("svg")
+   .attr("width", wide + 50)
+   .attr("height", high + 30);
+
+// create the Tooltip
 var tooltip = d3
-   .select(".visHolder")
+   .select("#data")
    .append("div")
    .attr("id", "tooltip")
    .style("opacity", 0);
 
-var overlay = d3
-   .select(".visHolder")
-   .append("div")
-   .attr("class", "overlay")
-   .style("opacity", 0);
+// Labels for X and Y axis
+svg.append("text")
+   .attr("transform", "rotate(-90)")
+   .attr("x", -(wide / 2.45))
+   .attr("y", 15)
+   .attr("class", "axis-label")
+   .text("Gross Domestic Product");
 
-var svgContainer = d3
-   .select(".visHolder")
-   .append("svg")
-   .attr("width", width + 100)
-   .attr("height", height + 60);
+svg.append("text")
+   .attr("x", wide / 1.78)
+   .attr("y", high - 10)
+   .attr("class", "axis-label")
+   .text("Year");
 
-d3.json(
-   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-)
-   .then((data) => {
-      svgContainer
-         .append("text")
-         .attr("transform", "rotate(-90)")
-         .attr("x", -200)
-         .attr("y", 80)
-         .text("Gross Domestic Product");
+const url =
+   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-      svgContainer
-         .append("text")
-         .attr("x", width / 2 + 120)
-         .attr("y", height + 50)
-         .text("More Information: http://www.bea.gov/national/pdf/nipaguid.pdf")
-         .attr("class", "info");
+// Get JSON data
+d3.json(url).then(function (data) {
+   // Updates headings
+   d3.select("#title").text(data.name);
+   // d3.select("#source_name").text(data.source_name);
 
-      var years = data.data.map(function (item) {
-         var quarter;
-         var temp = item[0].substring(5, 7);
+   // X Axis
+   var dates = data.data.map((v) => new Date(getData(v, 0)));
+   let maxX = new Date(findMax(dates));
+   maxX.setMonth(maxX.getMonth() + 3);
 
-         if (temp === "01") {
+   const xScale = d3
+      .scaleTime()
+      .domain([d3.min(dates), maxX])
+      .range([0, wide - 15]);
+
+   const xAxis = d3.axisBottom().scale(xScale);
+
+   svg.append("g")
+      .call(xAxis)
+      .attr("id", "x-axis")
+      .attr("transform", "translate(60, 490)");
+
+   // Y Axis
+   var gdp = data.data.map((v) => getData(v, 1));
+   const maxY = findMax(gdp);
+
+   const linearScale = d3
+      .scaleLinear()
+      .domain([0, maxY])
+      .range([0, high - 55]);
+
+   const yAxisScale = d3
+      .scaleLinear()
+      .domain([0, maxY])
+      .range([high - 55, 0]);
+
+   const yAxis = d3.axisLeft(yAxisScale);
+
+   svg.append("g")
+      .call(yAxis)
+      .attr("id", "y-axis")
+      .attr("transform", "translate(60, 5)");
+
+   const scaledGdp = gdp.map((v) => linearScale(v));
+
+   var years = data.data.map((v) => {
+      let quarter = "";
+
+      switch (v[0].substring(5, 7)) {
+         case "01":
             quarter = "Q1";
-         } else if (temp === "04") {
+            break;
+         case "04":
             quarter = "Q2";
-         } else if (temp === "07") {
+            break;
+         case "07":
             quarter = "Q3";
-         } else if (temp === "10") {
+            break;
+         case "10":
             quarter = "Q4";
-         }
+            break;
+         default:
+            quarter = "Error";
+      }
 
-         return item[0].substring(0, 4) + " " + quarter;
+      return v[0].substring(0, 4) + " " + quarter;
+   });
+
+   // Bars
+   d3.select("svg")
+      .selectAll("rect")
+      .data(scaledGdp)
+      .enter()
+      .append("rect")
+      .attr("data-date", (d, i) => data.data[i][0])
+      .attr("data-gdp", (d, i) => data.data[i][1])
+      .attr("data-quarter", (d, i) => years[i])
+      .attr("class", "bar")
+      .attr("x", (d, i) => xScale(dates[i]))
+      .attr("y", (d) => high - d)
+      .attr("width", barWidth)
+      .attr("height", (d) => d)
+      .attr("transform", "translate(60, -50)")
+      .on("mouseover", function (e) {
+         tooltip.transition().duration(0).style("opacity", 1);
+
+         tooltip
+            .html(
+               e.target.getAttribute("data-quarter") +
+                  "<br>" +
+                  "$" +
+                  window
+                     .Number(e.target.getAttribute("data-gdp"))
+                     .toFixed(1)
+                     .replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +
+                  " Billion"
+            )
+            .attr("data-date", e.target.getAttribute("data-date"))
+            .style("left", e.clientX + barWidth + "px")
+            .style("top", high - 80 + "px");
+      })
+      .on("mouseout", function () {
+         tooltip.transition().duration(0).style("opacity", 0);
       });
-
-      var yearsDate = data.data.map(function (item) {
-         return new Date(item[0]);
-      });
-
-      var xMax = new Date(d3.max(yearsDate));
-      xMax.setMonth(xMax.getMonth() + 3);
-      var xScale = d3
-         .scaleTime()
-         .domain([d3.min(yearsDate), xMax])
-         .range([0, width]);
-
-      var xAxis = d3.axisBottom().scale(xScale);
-
-      svgContainer
-         .append("g")
-         .calll(xAxis)
-         .attr("id", "x-axis")
-         .attr("transform", "translate(60, 400)");
-
-      var GDP = data.data.map(function (item) {
-         return item[1];
-      });
-
-      var scaledGDP = [];
-      var gdpMax = d3.max(GDP);
-      var linearScale = d3.scaleLinear().domain([0, gdpMax]).range([0, height]);
-      scaledGDP = GDP.map(function (item) {
-         return linearScale(item);
-      });
-
-      var yAxisScale = d3.scaleLinear().domain([0, gdpMax]).range([height, 0]);
-      var yAxis = d3.axisLeft(yAxisScale);
-
-      svgContainer
-         .append("g")
-         .call(yAxis)
-         .attr("id", "y-axis")
-         .attr("transform", "translate(60, 0)");
-
-      d3.select("svg")
-         .selectAll("rect")
-         .data(scaledGDP)
-         .enter()
-         .append("rect")
-         .attr("data-date", function (d, i) {
-            return data.data[i][0];
-         })
-         .attr("data-gdp", function (d, i) {
-            return data.data[i][1];
-         })
-         .attr("class", "bar")
-         .attr("x", function (d, i) {
-            return xScale(yearsDate[i]);
-         })
-         .attr("y", function (d) {
-            return height - d;
-         })
-         .attr("width", barWidth)
-         .attr("height", function (d) {
-            return d;
-         })
-         .attr("index", (d, i) => i)
-         .style("fill", "#33adff")
-         .attr("transform", "translate(60, 0)")
-         .on("mouseover", function (event, d) {
-            var i = this.getAttribute("index");
-
-            overlay
-               .transition()
-               .duration(0)
-               .style("height", d + "px")
-               .style("width", barWidth + "px")
-               .style("opacity", 0.9)
-               .style("left", i * barWidth + 0 + "px")
-               .style("top", height - d + "px")
-               .style("transform", "translate(60px)");
-            tooltip.transition().duration(200).style("opacity", 0.9);
-            tooltip
-               .html(
-                  years[i] +
-                     "<br>" +
-                     "$" +
-                     GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +
-                     " Billion"
-               )
-               .attr("data-date", data.data[i][0])
-               .style("left", i * barWidth + 30 + "px")
-               .style("top", height - 100 + "px")
-               .style("transform", "translate(60px)");
-         })
-         .on("mouseout", function () {
-            tooltip.transition().duration(200).style("opacity", 0);
-            overlay.transition().duration(200).style("opacity", 0);
-         });
-   })
-   .catch((e) => console.log(e));
+});
