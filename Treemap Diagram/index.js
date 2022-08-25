66fd5ded96a19a0d8f513e8df138065eaa3dc4e4
Ian Miller <theiandmiller@gmail.com>
2022-08-25 05:53:47 -0500

    treemap updates

--- Treemap Diagram/index.js
+++ Treemap Diagram/index.js
@@ -6,10 +6,32 @@ async function run() {
    );
    const kickstarter = await kickRes.json();
 
+   // const countiesResp = await fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json');
+   // const counties = await countiesResp.json();
+
    const width = 960;
    const height = 600;
+   // const padding = 60;
 
    const color = d3.scaleOrdinal(d3.schemeCategory10);
+   // color scheme with 10 catergorys more created though better then assigning colors indvidiually
+
+   //   const path = d3.geoPath();
+
+   //   const data = topojson.feature(counties, counties.objects.counties).features;
+
+   //   const minEdu = d3.min(educations, edu => edu.bachelorsOrHigher);
+   //   const maxEdu = d3.max(educations, edu => edu.bachelorsOrHigher);
+   //   const step = ((maxEdu - minEdu) / 8);
+
+   //   const colorsScale = d3.scaleThreshold()
+   //   .domain(d3.range(minEdu, maxEdu, step))
+   //            .range(d3.schemePurples[9])
+   //   const colors = [];
+
+   //   for(let i=minEdu; i<=maxEdu; i+= step) {
+   //     colors.push(colorsScale(i));
+   //   }
 
    const svg = d3
       .select("#container")
@@ -28,7 +50,7 @@ async function run() {
       .data(root.leaves())
       .enter()
       .append("g")
-      .attr("transform", (d) => `translate(${(d, x0)}, ${(d, y0)})`);
+      .attr("transform", (d) => `translate(${d.x0}, ${d.y0})`); //x0 and y0 top left cornor of boxes
 
    const tile = cell
       .append("rect")
@@ -36,29 +58,31 @@ async function run() {
       .attr("data-name", (d) => d.data.name)
       .attr("data-category", (d) => d.data.category)
       .attr("data-value", (d) => d.data.value)
-      //   .attr('id', d => d.data.id)
-      .attr("width", (d) => d.x1 - d.x0)
-      .attr("height", (d) => (d.y1 = d.y0))
-      .attr("fill", (d) => color(d.data.category))
+      .attr("width", (d) => d.x1 - d.x0) //button right cornor
+      .attr("height", (d) => d.y1 - d.y0) // button right lower w
+      .attr("fill", (d) => color(d.data.category)) // pass in a color function wil give you a color for catorgory passed in
       .on("mouseover", (d, i) => {
          const { name, category, value } = d.data;
-         tooltip.classLise.add("show");
+         tooltip.classList.add("show");
          tooltip.style.left = d3.event.pageX + "px";
          tooltip.style.top = d3.event.pageY - 100 + "px";
          tooltip.setAttribute("data-value", value);
+
          tooltip.innerHTML = `
-            <p><strong>Name: </strong>${name}</p>
-            <p><strong>Category: </strong>${category}</p>
-            <p><strong>Value: </strong>${value}</p>`;
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Category:</strong> ${category}</p>
+        <p><strong>Value:</strong> ${value}</p>
+      `;
       })
       .on("mouseout", () => {
          tooltip.classList.remove("show");
       });
 
+   //displays all texts for
    cell
       .append("text")
       .selectAll("tspan")
-      .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+      .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g)) //loops over and uses regex to find uppercase letters
       .enter()
       .append("tspan")
       .attr("style", "font-size: 13px")
@@ -69,19 +93,29 @@ async function run() {
    const categories = root
       .leaves()
       .map((n) => n.data.category)
-      .filter((item, idx, arr) => arr.indexOf(item) === idx);
+      .filter((item, idx, arr) => arr.indexOf(item) === idx); // removes duplicated product designs
 
    const blockSize = 20;
    const legendWidth = 200;
    const legendHeight = (blockSize + 2) * categories.length;
 
+   // const legendRectWidth = legendWidth / colors.length;
    const legend = d3
       .select("body")
       .append("svg")
       .attr("id", "legend")
-      //      .attr("class", "legend")
       .attr("width", legendWidth)
       .attr("height", legendHeight);
+
+   // .selectAll('rect')
+   // .data(colors)
+   // .enter()
+   // .append('rect')
+   // .attr('x', (_,i) => i * legendRectWidth)
+   // .attr('y', 0)
+   // .attr('width', legendRectWidth)
+   // .attr('height', legendHeight)
+   // .attr('fill', c=> c)
 
    legend
       .selectAll("rect")
